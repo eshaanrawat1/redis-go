896863a1dabfdd8df335668accdf7d8cd784c95e
Eshaan Rawat <erawat@uci.edu>
2026-07-16 22:50:01 -0700

    tcp server

--- listener.go
+++ listener.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		recvBuf := make([]byte, 1024)
+
+		_, err := conn.Read(recvBuf)
+		if err != nil {
+			fmt.Println("Error reading from TCP stream: ", err)
+		}
+
+		fmt.Println(recvBuf)
+	}
+
+}
+
+func Listen() {
+	ln, err := net.Listen("tcp", ":5284")
+	if err != nil {
+		fmt.Println("Error happened: ", err)
+	}
+	fmt.Println("Server is listening...")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error in accepting connection: ", err)
+		}
+
+		fmt.Println("Accepted a TCP connection")
+		go handleConnection(conn)
+	}
+}
--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func parser() {
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
-
-		cmds := strings.Split(text, " ")
-
-		if cmds[0] == "Quit" {
-			break
-		}
-
-		if cmds[0] == "Hi" {
-			fmt.Println("hello there")
-		}
-	}
-}
-
 func main() {
-	parser()
+	Listen()
 }
